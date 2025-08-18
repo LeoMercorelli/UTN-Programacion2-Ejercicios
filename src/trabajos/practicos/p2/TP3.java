4cee1b966101b41c25d9d472c67d82fd73f260bd
Leonel Mercorelli <leonelmercorelli@hotmail.com>
2025-08-17 22:29:05 -0300

    TP2

---
+++ src/trabajos/practicos/p2/TP3.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajos.practicos.p2;
+
+public class TP3 {
+    public static void main(String[] args) {
+        System.out.println("Soy el TP3");
+    }
+}
